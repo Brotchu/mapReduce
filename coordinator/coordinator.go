3d6dd531548c3448302b48fd6e03a39978ffb8e0
Brotchu <nithint216@gmail.com>
2021-02-21 19:00:45 +0530

    added polling of workers

--- coordinator/coordinator.go
+++ coordinator/coordinator.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"net/rpc"
 	"os"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -14,36 +16,65 @@ func main() {
 	var api = newAPI()
 	must(rpc.Register(api))
 	rpc.HandleHTTP()
-
+	go pollWorker(api)
 	lis, err := net.Listen("tcp", ":4040")
 	must(err)
 
 	fmt.Println("Coordinator started on 4040")
 	must(http.Serve(lis, nil))
-}
 
-// type worker struct {
-// 	addr string
-// 	status string
-// }
+	//TODO: where to start worker check
+}
 
 //API server struct
 type API struct {
+	done   bool
 	worker map[string]string //map [addr] -> status
+	Mut    *sync.Mutex
 }
 
 func newAPI() *API {
 	return &API{
+		Mut:    &sync.Mutex{},
+		done:   false,
 		worker: make(map[string]string),
 	}
 }
 
 //RegisterWorker : rpc for worker to register
-func (a *API) RegisterWorker(addr string, reply *bool) error {
+func (a *API) RegisterWorker(addr string, reply *string) error { //reply *ResgisterResponse
+
+	a.Mut.Lock()
 	a.worker[addr] = "idle" //idle, working, complete
+	a.Mut.Unlock()
+
 	fmt.Printf("Wokers : %+v\n", a.worker)
-	*reply = true
+	*reply = "./plugintest.so"
+
 	return nil
+}
+
+func pollWorker(a *API) {
+	for {
+		for k, _ := range a.worker {
+			//poll rpc of worker
+			_, err := rpc.DialHTTP("tcp", k) //pollClient //TODO: is commented part really needed ?
+			// if err != nil {
+			// 	delete(a.worker, k)
+			// }
+			// var res bool
+			// err = pollClient.Call("", true, &res)
+			if err != nil {
+				fmt.Println("Error during poll : ", err)
+				a.Mut.Lock()
+				delete(a.worker, k)
+				a.Mut.Unlock()
+			}
+
+		}
+		fmt.Printf("Workers [Poll] : %+v\n", a.worker)
+		time.Sleep(10 * time.Second)
+	}
 }
 
 func must(err error) {
--- worker/worker.go
+++ worker/worker.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"net/rpc"
 	"os"
+	"plugin"
 	"strings"
 )
+
+// type API int
+
+// func (a *API) Ack()
 
 func main() {
 	//os args 1- local port to use ; 2- addr of coordinator
@@ -22,11 +28,16 @@ func main() {
 	client, err := rpc.DialHTTP("tcp", coordinatorAddr)
 	must(err)
 
-	var reply bool
+	var reply string //get function name
 	err = client.Call("API.RegisterWorker", getLocalAddr()+":"+localPort, &reply)
 	must(err)
 	fmt.Println("Worker registered")
+
+	workerFunc := loadPlugin(reply)
+	workerFunc()
 	//start go routine to reply to coordinator checks
+	//TODO:
+	go listenForPoll(localPort)
 
 	//Testing : waiting indefinitely
 	<-make(chan int)
@@ -40,6 +51,34 @@ func getLocalAddr() string {
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	addrString := strings.Split(localAddr.String(), ":")
 	return addrString[0]
+}
+
+func loadPlugin(pName string) func() {
+	p, err := plugin.Open(pName)
+	must(err)
+
+	f, err := p.Lookup("TestDisplay")
+	funcTest := f.(func())
+	return funcTest
+}
+
+type WorkerAPI struct{}
+
+func (w *WorkerAPI) Poll(check bool, reply *bool) error {
+	*reply = true
+	return nil
+}
+
+func listenForPoll(port string) {
+	var wAPI = &WorkerAPI{}
+	must(rpc.Register(wAPI))
+	rpc.HandleHTTP()
+
+	lis, err := net.Listen("tcp", ":"+port)
+	must(err)
+
+	must(http.Serve(lis, nil))
+
 }
 
 func must(err error) {
